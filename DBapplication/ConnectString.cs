352e0ce681c19be6bd545197db5b3f3d3fa8c33f
yousefsawy <99139949+yousefsawy@users.noreply.github.com>
2023-12-10 12:11:26 +0200

    Modified the Connection string

---
+++ DBapplication/ConnectString.cs
@@ -0,0 +1,7 @@
+﻿namespace DBapplication
+{
+    public static class ConnectString
+    {
+        public static string Val = @"Data Source=YOUSEF_HP\SQLEXPRESS;Initial Catalog=project_standard;Integrated Security=True";
+    }
+}
