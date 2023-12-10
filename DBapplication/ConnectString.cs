8b88d20ce900fe805447ea04184fffd271cf7062
yousefsawy <99139949+yousefsawy@users.noreply.github.com>
2023-12-10 16:59:03 +0200

    test

--- DBapplication/ConnectString.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace DBapplication
-{
-    public static class ConnectString
-    {
-        public static string Val = @"Data Source=YOUSEF_HP\SQLEXPRESS;Initial Catalog=project_standard;Integrated Security=True";
-        //TEST
-    }
-}
