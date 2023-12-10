e0d99a601c078255534949f968af63625904c1db
yousefsawy <99139949+yousefsawy@users.noreply.github.com>
2023-12-10 11:42:30 +0200

    Add project files.

---
+++ DBapplication/Controller.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using System.Windows.Forms;
+
+namespace DBapplication
+{
+    public class Controller
+    {
+        DBManager dbMan;
+        public Controller()
+        {
+            dbMan = new DBManager();
+        }
+        
+        //insert functions here
+      
+        public void TerminateConnection()
+        {
+            dbMan.CloseConnection();
+        }
+       
+    }
+}
